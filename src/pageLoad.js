3b2b34ca7b488222455a0a0d0ab0bf051c13094d
BlueVelocity <josephrodney@hotmail.com>
2023-12-18 16:39:21 -0500

    Display landingComponent using custom module

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,33 +1,22 @@
+import pageManipulation from './pageManipulation';
 import chefImage from './images/landing-page-chef.gif';
-
-function imgComponent() {
-    const titleImg = new Image();
-    titleImg.src = chefImage;
-    titleImg.setAttribute('width', '500px')
-
-    return titleImg;
-}
-
-function titleComponent() {
-    const element = document.createElement('h1');
-    element.textContent = 'Spaghettorama!'
-
-    return element;
-}
-
-function textComponent() {
-    const element = document.createElement('p');
-    element.textContent = 'This pasta will Mama your Mia';
-
-    return element;
-}
-
-//Accepts an Id | string as fist argument, followed by the elements to append as child
-function appendElementByID(Id, ...args) {
-    args.forEach(element => document.getElementById(Id).appendChild(element))
-}
+import './style.css';
 
 export default function pageLoad() {
     console.log('pageLoad is working!');
-    appendElementByID('content', imgComponent(), titleComponent(), textComponent())
+    pageManipulation.appendElementByID('content', landingComponent())
+}
+
+function landingComponent() {
+        const titleImg = new Image();
+        titleImg.src = chefImage;
+        titleImg.setAttribute('width', '500px')
+
+        const headerElement = document.createElement('h1');
+        headerElement.textContent = 'Spaghettorama!'
+
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = 'This pasta will Mama your Mia';
+
+    return [ titleImg, headerElement, descriptionElement ]
 }
