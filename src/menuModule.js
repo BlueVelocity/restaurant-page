49fb5e412e15d4f199a311fa2760719d78a42ba6
BlueVelocity <josephrodney@hotmail.com>
2023-12-19 18:00:43 -0500

    Show menu and extra's on menu tab

--- src/menuModule.js
+++ src/menuModule.js
@@ -1,4 +1,6 @@
 import utility from './utility';
+import meme from './images/permanence_meme.jpg'
+import meatball from './images/meatball.png'
 
 export default function loadMenuComponent() {
     utility.appendElementByID('content', menuComponent());
@@ -9,10 +11,10 @@ function menuComponent() {
     headerElement.textContent = 'Menu';
 
     const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = 'This menu is random like the noodles in pasta'
+    descriptionElement.textContent = 'This menu is random like the noodles in pasta';
 
     const justKeepSlapping = document.createElement('p');
-    justKeepSlapping.textContent = 'Just keep slapping that menu tab if you want to randomize'
+    justKeepSlapping.textContent = 'Just keep slapping that menu tab if you like chaos, the kitchen staff won\'t mind';
 
     function randomMenuItems() {
         const mains = ['Spaghetti', 'Pasta', 'Noodles', 'Ramen', 'Vermicelli'];
@@ -29,5 +31,24 @@ function menuComponent() {
     menuList.classList = 'menu-list';
     randomMenuItems().forEach(menuItem => menuList.appendChild(menuItem))
 
-    return [ headerElement, descriptionElement, justKeepSlapping, menuList ]
+    //styling done here for the meme
+    const permananceMemeball  = new Image();
+    permananceMemeball.classList = 'clickable-image';
+    permananceMemeball.setAttribute('visibility', 'hidden');
+    permananceMemeball.setAttribute('style', 'width: 8rem; margin-top: 1rem');
+    permananceMemeball.setAttribute('hover', 'pointer');
+    permananceMemeball.src = meatball;
+
+    permananceMemeball.addEventListener('click', function memeListener(){
+        permananceMemeball.src = meme;
+        permananceMemeball.classList = null;
+        permananceMemeball.setAttribute('style', 'width: 20rem; border-radius: 1rem; margin-top: 1rem');
+        permananceMemeball.removeEventListener('click', memeListener)
+
+        const itsFree = document.createElement('p');
+        itsFree.textContent = 'The food looks free, but it\'ll still cost you :)';
+        utility.appendElementByID('content', [ itsFree ])
+    })
+
+    return [ headerElement, descriptionElement, justKeepSlapping, menuList, permananceMemeball]
 }
